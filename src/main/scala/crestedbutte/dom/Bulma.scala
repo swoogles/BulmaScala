9bb69f75291e65d84fe542771315c69cbd41f7ed
bfrasure <bill.frasure@gmail.com>
2020-04-11 17:26:05 -0600

    Create basic menu

--- src/main/scala/crestedbutte/dom/Bulma.scala
+++ src/main/scala/crestedbutte/dom/Bulma.scala
@@ -19,38 +19,34 @@ object Bulma {
       button(cls := "modal-close is-large", aria.label := "close")(),
     )
 
-//  def menu(allComponentData: Seq[ComponentData]) =
-//    div(id := "main-menu",
-//        cls := "navbar",
-//        role := "navigation",
-//        aria.label := "main navigation")(
-//      div(cls := "navbar-brand")(
-//        a(role := "button",
-//          cls := "navbar-burger burger",
-//          aria.label := "menu",
-//          aria.expanded := "false",
-//          data("target") := "navbarBasicExample")(
-//          span(aria.hidden := "true"),
-//          span(aria.hidden := "true"),
-//          span(aria.hidden := "true"),
-//        ),
-//      ),
-//      div(id := "navbarBasicExample", cls := "navbar-menu")(
-//        div(cls := "navbar-start")(
-//          div(cls := "navbar-item has-dropdown is-hoverable")(
-//            a(cls := "navbar-link")("Restaurants"),
-//            div(cls := "navbar-dropdown")(
-//              allComponentData.map { componentData =>
-//                a(
-//                  cls := "navbar-item route",
-//                  data("route") := componentData.componentName,
-//                )(componentData.namedRoute.routeName.userFriendlyName)
-//              },
-//            ),
-//          ),
-//        ),
-//        div(cls := "navbar-end")(
-//          ),
-//      ),
-//    )
+  def menu(choices: Seq[JsDom.TypedTag[Div]]) =
+    div(id := "main-menu",
+        cls := "navbar",
+        role := "navigation",
+        aria.label := "main navigation")(
+      div(cls := "navbar-brand")(
+        a(role := "button",
+          cls := "navbar-burger burger",
+          aria.label := "menu",
+          aria.expanded := "false",
+          data("target") := "navbarBasicExample")(
+          span(aria.hidden := "true"),
+          span(aria.hidden := "true"),
+          span(aria.hidden := "true"),
+        ),
+      ),
+      div(id := "navbarBasicExample", cls := "navbar-menu")(
+        div(cls := "navbar-start")(
+          div(cls := "navbar-item has-dropdown is-hoverable")(
+            a(cls := "navbar-link")("Restaurants"),
+            div(cls := "navbar-dropdown")(
+              choices
+            ),
+          ),
+        ),
+        div(cls := "navbar-end")(
+          ),
+      ),
+    )
+
 }
